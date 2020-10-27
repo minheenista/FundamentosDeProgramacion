f3419651de91d36a12845a7d8fe2b5a760011766
Mich <Mich@DESKTOP-M1SDKQI>
2020-10-27 15:58:41 -0600

    Clase 201027

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+// definicion y prototipos funciones
+
+//variables y constantes
+
+// implementacion de funciones
+
+
+int main(int argc, char** argv) {
+    
+    int x=1709;
+    
+    
+
+    printf("Minhee te amo");
+    
+    return (EXIT_SUCCESS);
+}
+
